adf439eaf0a1c3cf7764d635565b11876a12aab9
jmoldon <jmoldon@iaa.es>
2020-03-14 17:53:43 +0100

    Add webpage

--- convert_pdf.sh
+++ convert_pdf.sh
@@ -1,1 +1,2 @@
 jupyter nbconvert  --no-input --to pdf --output cvid_report.pdf "Coronavirus Analysis.ipynb"
+jupyter nbconvert  --no-input --to html --output cvid_report.html "Coronavirus Analysis.ipynb"
