c06b260b64c45c7db399175fc9444fc249353df0
jmoldon <jmoldon@iaa.es>
2020-03-14 17:36:20 +0100

    More details in the analysis, including prediction

--- convert_pdf.sh
+++ convert_pdf.sh
@@ -1,1 +1,1 @@
-jupyter nbconvert  --no-input --to pdf --output cvid_report_2020-03-14.pdf coronavirus_analysis_v0_2020-03-14.ipynb
+jupyter nbconvert  --no-input --to pdf --output cvid_report.pdf "Coronavirus Analysis.ipynb"
